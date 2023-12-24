02160b66714dba2ae1dcdd381aa8ddf87203d023
DushanMalind <dushanmalinda010@gmail.com>
2023-12-24 23:33:34 +0530

    Rooms images add 1

--- client/src/view/page/room/Room.tsx
+++ client/src/view/page/room/Room.tsx
@@ -1,5 +1,12 @@
 import {Component} from "react";
-
+import room1 from "../../../images/room1.jpg"
+import room2 from "../../../images/room2.jpg"
+import room3 from "../../../images/room3.jpg"
+import room4 from "../../../images/room4.jpg"
+import room5 from "../../../images/room5.jpg"
+import room6 from "../../../images/room6.jpg"
+import room7 from "../../../images/room7.jpg"
+import room8 from "../../../images/room8.jpg"
 
 
 export class Room extends Component {
