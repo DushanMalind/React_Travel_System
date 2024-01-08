20c2ec4d2857715a59b62256c0bccf75c8f9c6b8
DushanMalind <dushanmalinda010@gmail.com>
2024-01-08 15:17:20 +0530

    sign Form filed

--- client/src/view/page/signIn/SignIn.tsx
+++ client/src/view/page/signIn/SignIn.tsx
@@ -27,7 +27,7 @@ export class SignIn extends Component {
                                                     First Name
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="firstName"
                                                     type="text"
                                                     placeholder="First Name"/>
@@ -38,7 +38,7 @@ export class SignIn extends Component {
                                                     Last Name
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="lastName"
                                                     type="text"
                                                     placeholder="Last Name"
@@ -52,7 +52,7 @@ export class SignIn extends Component {
                                                     Address
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="firstName"
                                                     type="text"
                                                     placeholder="Address"/>
@@ -63,7 +63,7 @@ export class SignIn extends Component {
                                                     Contact
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="lastName"
                                                     type="text"
                                                     placeholder="Contact"
@@ -89,7 +89,7 @@ export class SignIn extends Component {
                                                     Password
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="password"
                                                     type="password"
                                                     placeholder="******************"
@@ -102,7 +102,7 @@ export class SignIn extends Component {
                                                     Confirm Password
                                                 </label>
                                                 <input
-                                                    className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                                    className="w-full px-10 py-2 mb-3 text-sm leading-tight text-gray-700 dark:text-white border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                                                     id="c_password"
                                                     type="password"
                                                     placeholder="******************"
