afb42edb75bec4549e864bbb89248e5bfe6a3782
DushanMalind <dushanmalinda010@gmail.com>
2024-01-13 21:42:19 +0530

    card Update

--- client/src/view/page/admin/Admin.tsx
+++ client/src/view/page/admin/Admin.tsx
@@ -237,8 +237,8 @@ export class Admin extends Component {
                     </div>
                     <div className="container mx-auto mt-12">
                         <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3">
-                            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-                                <div className="text-sm font-medium text-gray-500 truncate">
+                            <div className="w-full px-4 py-5  rounded-lg shadow bg-cyan-500 shadow-red-600">
+                                <div className="text-md font-medium text-gray-950 truncate">
                                     Total users
                                 </div>
                                 <div className="mt-1 text-3xl font-semibold text-gray-900">
