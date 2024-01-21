2d585be78e0fc037c2c7b4ffbabfb4b9bdddc8b4
DushanMalind <dushanmalinda010@gmail.com>
2024-01-21 19:49:50 +0530

    Room Card Update

--- client/src/view/common/RoomAcceptProduct/RoomAcceptProduct.tsx
+++ client/src/view/common/RoomAcceptProduct/RoomAcceptProduct.tsx
@@ -80,6 +80,7 @@ export class RoomAcceptProduct extends Component <RoomAcceptProductProps,RoomAcc
                     </div>
                 </div>*/}
 
+
                  <div className="py-4 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
                     <div
                         className="mt-4 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-2 space-y-4 md:space-y-6 xl:space-y-0">
